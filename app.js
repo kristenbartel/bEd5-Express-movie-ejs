8adda8d8a41c1eb6d1e68e3aad961fa8a931ef59
Kristen Bartel <kristenbartel@gmail.com>
2022-02-10 21:26:13 -0600

    fingured out styles.css link

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ app.use(morgan("tiny"));
 
 // view engine setup with ejs instead of html
 app.set("view engine", "ejs")
+app.set("views", "views")
 
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 const db = require("./Models/db");
 
@@ -17,7 +18,7 @@ app.use('/', indexRoute) //or app.use('/', require('./routes/index'))
 
 app.use('/shows', require('./routes/shows'))
 
-app.use('/showDetails/:movieDetails', require('./routes/showDetails'))
+app.use('/showDetails', require('./routes/showDetails'))
 
 app.listen(port, () => {
   console.log(`YO JOE!  Coming in HOT on http://localhost:${port}`);
--- routes/showDetails.js
+++ routes/showDetails.js
@@ -3,10 +3,33 @@ const router = express.Router();
 
 const db = require('../models/db')
 
-router.get('/', (req, res) => {
+router.get('/:details', (req, res) => {
+    // filter here with .find()
     res.render('partials/showDetailsP', {
             db: db,
     })
 })
 
 module.exports = router
+
+  router.get('/:Title', (req, res) => {
+    const {Title} = req.params;
+    const show = db.find(f => f.Title === Title);
+  
+
+    if(show) {
+         res.render('index', {
+              locals: {
+                   showsData: show,
+              }, 
+              partials: {
+                 listButton: '/partials/show-details'
+
+              }
+         },
+         )
+    } else {
+       res.status(404).send(`no friend with handle ${Title}`)
+           
+   }
+})
